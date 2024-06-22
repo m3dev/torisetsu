c4bf532cdf1262d8065e4105b10628c09a7527f3
河合 俊典 <6syun9@gmail.com>
2024-06-22 09:11:27 +0900

    update share button

--- torisetsu/src/jsMain/kotlin/components/base/XShareButton.kt
+++ torisetsu/src/jsMain/kotlin/components/base/XShareButton.kt
@@ -1,6 +1,7 @@
 package components.base
 
 import androidx.compose.runtime.Composable
+import kotlinx.browser.window
 import org.jetbrains.compose.web.attributes.ATarget
 import org.jetbrains.compose.web.attributes.target
 import org.jetbrains.compose.web.dom.A
@@ -11,7 +12,7 @@ import org.jetbrains.compose.web.dom.Text
 @Composable
 fun XShareButton() {
     A(
-        href = "https://twitter.com/intent/tweet?text=私のトリタイプが見つかりました！&url=https://m3dev.github.io/torisetsu&hashtags=エンジニアトリ診断",
+        href = "https://twitter.com/intent/tweet?text=私のトリタイプが見つかりました！&url=" + window.location.href + "&hashtags=エンジニアトリ診断",
         attrs = {
             target(ATarget.Blank)
             classes(XShareButtonStyle.rootElm)
